6c4fe538f7c564caa560a6b9ee3d683318e063b8
Md Shamim <shamimice03@gmail.com>
2023-07-02 20:27:39 +0900

    json updated

--- working-with-json/main.go
+++ working-with-json/main.go
@@ -5,5 +5,10 @@ import "fmt"
 func main() {
 
 	videos := getVideos()
+	for k, _ := range videos {
+
+		videos[k].Description = videos[k].Description + "\n Adding Des"
+	}
 	fmt.Println(videos)
+	saveVideos(videos)
 }
--- working-with-json/videos.go
+++ working-with-json/videos.go
@@ -1,29 +1,68 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type video struct {
-	Id          string
-	Title       string
-	Description string
-	Imageurl    string
+	Id          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Imageurl    string `json:"imageurl"`
+	Url         string `json:"url"`
 }
 
 func getVideos() (videos []video) {
 
-	v1 := video{
-		Id:          "23223",
-		Title:       "Hello Image",
-		Description: "Very Nice",
-		Imageurl:    "http://goodday.com",
+	filebyets, err := ioutil.ReadFile("./videos.json")
+
+	if err != nil {
+		panic(err)
 	}
-	v2 := video{
-		Id:          "34646",
-		Title:       "Good Image",
-		Description: "Nice View",
-		Imageurl:    "http://baybay.com",
+
+	err = json.Unmarshal(filebyets, &videos)
+
+	if err != nil {
+		panic(err)
 	}
-	videos = append(videos, v1)
-	videos = append(videos, v2)
+
 	return videos
 
+	// convert the byte to string
+	// fileContent := string(filebyets)
+	// fmt.Printf(fileContent)
+	// return nil
+
+	// manually insert data to the struct (video)
+
+	// v1 := video{
+	// 	Id:          "23223",
+	// 	Title:       "Hello Image",
+	// 	Description: "Very Nice",
+	// 	Imageurl:    "http://goodday.com",
+	// 	Url:         "http://test.com",
+	// }
+	// v2 := video{
+	// 	Id:          "34646",
+	// 	Title:       "Good Image",
+	// 	Description: "Nice View",
+	// 	Imageurl:    "http://baybay.com",
+	// 	Url:         "http://testting.com",
+	// }
+	// videos = append(videos, v1)
+	// videos = append(videos, v2)
+	// return videos
+
 	/// return []video{v1, v2}  # can be written like this
+}
+
+func saveVideos(videos []video) {
+	videoBytes, err := json.Marshal(videos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	ioutil.WriteFile("./videos-updated.json", videoBytes, 0644)
 }
